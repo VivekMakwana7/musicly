51e12a632acadf36be79bf6834df7366fd1783d9
vivek <vivek.makvana@codezeros.com>
2025-03-25 09:48:41 +0530

    - Merge conflict solved

--- lib/src/search/widgets/song_search_widget.dart
+++ lib/src/search/widgets/song_search_widget.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:musicly/core/constants.dart';
+import 'package:musicly/core/cubits/audio/audio_cubit.dart';
+import 'package:musicly/core/db/data_base_handler.dart';
+import 'package:musicly/core/db/models/song/db_song_model.dart';
+import 'package:musicly/core/di/injector.dart';
 import 'package:musicly/core/extensions/ext_build_context.dart';
+import 'package:musicly/core/extensions/ext_string_alert.dart';
 import 'package:musicly/routes/app_router.dart';
-import 'package:musicly/src/search/cubit/search_cubit.dart';
 import 'package:musicly/src/search/model/song/global_song_model.dart';
 import 'package:musicly/widgets/song_item_widget.dart';
 
 /// Song Search Widget
 class SongSearchWidget extends StatelessWidget {
   /// Song Search Widget Constructor
-  const SongSearchWidget({super.key, this.songs = const [], this.query});
+  const SongSearchWidget({super.key, this.globalSongs, this.query, this.dbSongs});
 
-  /// List of Songs widget
-  final List<GlobalSongModel> songs;
+  /// Constructor for displaying songs from the local database.
+  factory SongSearchWidget.fromDatabase(List<DbSongModel> songs) {
+    return SongSearchWidget(dbSongs: songs);
+  }
 
-  /// For pass it in Search Song page
+  /// Constructor for displaying songs from a global source.
+  factory SongSearchWidget.api({required List<GlobalSongModel> songs, String? query}) {
+    return SongSearchWidget(globalSongs: songs, query: query);
+  }
+
+  /// List of songs from a global source.
+  final List<GlobalSongModel>? globalSongs;
+
+  /// The search query used to find these songs (if applicable).
   final String? query;
+
+  /// List of songs from the local database.
+  final List<DbSongModel>? dbSongs;
 
   @override
   Widget build(BuildContext context) {
--- lib/src/search/widgets/top_trending_search_widget.dart
+++ lib/src/search/widgets/top_trending_search_widget.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:musicly/core/cubits/audio/audio_cubit.dart';
 import 'package:musicly/core/db/data_base_handler.dart';
 import 'package:musicly/core/di/injector.dart';
-import 'package:musicly/core/db/data_base_handler.dart';
 import 'package:musicly/core/enums/search_item_type.dart';
 import 'package:musicly/core/extensions/ext_build_context.dart';
+import 'package:musicly/src/search/cubit/search_cubit.dart';
 import 'package:musicly/src/search/model/top_trending/global_top_trending_model.dart';
 import 'package:musicly/widgets/album_item_widget.dart';
 import 'package:musicly/widgets/artist_item_widget.dart';
